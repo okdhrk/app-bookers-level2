2fc1c35ccf7659c47a77b7d1f439a5671ce6bd11
okdhrk <okdhrk1014@gmail.com>
2021-02-03 07:40:34 +0000

    follow view

--- app/controllers/follow_relationships_controller.rb
+++ app/controllers/follow_relationships_controller.rb
@@ -2,7 +2,7 @@ class FollowRelationshipsController < ApplicationController
 
   def create
     @user =User.find(params[:follow_relationship][:following_id])
-    current_user.follow(@user)
+    current_user.fsollow(@user)
     respond_to do |format|
       format.html {redirect_back(fallback_location: root_url)}
       format.js
